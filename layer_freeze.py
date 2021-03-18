dc5a4720a29dfb6cc31b0450f1c140f6e05fe927
zhaolu <zhaolu16120284@163.com>
2021-03-18 13:02:39 +0800

    layer freeeze

---
+++ layer_freeze.py
@@ -0,0 +1,5 @@
+for k, v in model.named_parameters():
+    v.requires_grad = True  # train all layers
+    if any(x in k for x in freeze):
+        print('freezing %s' % k)
+        v.requires_grad = False
